fa99658944c03b16abf855759db6780e87985fa1
Matt Greer <matt.e.greer@gmail.com>
2014-11-23 16:11:31 -0700

    pg: using env based initialization from newest version of the Docker image fixes #17

--- docker/pg/create-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "******CREATING TESTDB DATABASE******"
-gosu postgres postgres --single <<- EOSQL
-   CREATE DATABASE testdb;
-   CREATE USER testdb with password 'password';
-   GRANT ALL PRIVILEGES ON DATABASE testdb to testdb;
-EOSQL
-echo ""
-echo "******TESTDB DATABASE CREATED******"
